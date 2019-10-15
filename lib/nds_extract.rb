3edbf4b6c9c72723cf88b9ccbb90021c71f07b9b
Romario Gustave <romagustave10@gmail.com>
2019-10-15 04:21:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,9 +8,11 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
   nil
+  directors =[{:name "Paul", :revenue => 123456789}, {:name "Jean" :revenue => 1234567789}, {:name "Sam" :revenue =>1234567889}]
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  
 end
